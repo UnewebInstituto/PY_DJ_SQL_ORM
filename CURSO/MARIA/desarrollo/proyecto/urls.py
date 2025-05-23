0c250372085144cef0537080399b1f43f8cb775c
HENRY DUQUE <hduqueuneweb@gmail.com>
2025-05-23 12:47:18 -0400

    clase 2

---
+++ CURSO/MARIA/desarrollo/proyecto/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for proyecto project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from proyecto.views import hola , otro_hola, obtener_fecha_hora
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("saludo/", hola),
+    path("otrosaludo/", otro_hola),
+    path("fechahora/", obtener_fecha_hora)
+]
---
+++ CURSO/MARIA/desarrollo/proyecto/views.py
@@ -0,0 +1,42 @@
+from django.http import HttpResponse
+import datetime
+def hola(request):
+  
+  return HttpResponse("<h1>Hola estimados alumnos de UNEWEB </h1>")
+
+def otro_hola(request):
+  CONTENIDO_HTML= """
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Otro Saludo</title>
+  <style>
+    h1{
+      text-align: center;
+      padding: 20px;
+      background-color: #ccc;
+      color: blue;
+      border-radius: 25px;
+      border-style: solid;
+      border-width: 2px;
+      border-color: purple;
+      font-family: Arial, Helvetica, sans-serif;
+      margin-top: 20px;
+    }
+    </style>
+</head>
+<body>
+  <h1>hola este es otro saludo,para los estudiante de Uneweb</h1>
+  
+</body>
+</html>
+
+  """
+  return HttpResponse(CONTENIDO_HTML)
+
+def obtener_fecha_hora(request):
+  ahora = datetime.datetime.now() 
+  mostrar_ahora = "<h2>" + str(ahora) +"</h2>"
+  return HttpResponse(mostrar_ahora)
